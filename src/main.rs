aec084125f2d037edeb2f7b8b78222758a51e3d1
Enzo Queiroz Costa <enzo415611@gmail.com>
2025-09-11 13:39:08 -0300

    Add custom window icon and update app title
    Added OI.ico as the application icon and set it in the window options. Updated the window title from 'Editor' to 'Text Editor' in main.rs for better clarity.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::{path::PathBuf};
-use eframe::{ NativeOptions};
+use std::{path::PathBuf, sync::Arc};
+use eframe::{NativeOptions};
 
 mod app;
 
 
 // ponto de entrada do programa
-fn main() {    
-   _ = eframe::run_native(
-        "Editor",
-         NativeOptions::default(),
-          Box::new(|cc| Ok(Box::new(Editor::new(cc)))));
+fn main() {
+    let d = eframe::icon_data::from_png_bytes(include_bytes!("../assets/OI.ico"))
+    .expect("The icon data must be valid");
+    let mut native_options = NativeOptions::default();
+    native_options.viewport.icon = Some(Arc::new(d));
+
+    _ = eframe::run_native(
+        "Text Editor",
+        native_options,
+        Box::new(|cc| Ok(Box::new(Editor::new(cc)))));
 }
 
 
